2bad30212e653fe65787711d55c5d4f109720d35
root <root@fabulousbeast.(none)>
2010-04-28 06:17:08 +0000

    got js animation of title fully working

--- public/javascript/jquery.rotatetitle.js
+++ public/javascript/jquery.rotatetitle.js
@@ -11,24 +11,25 @@
 (function($){
   $.fn.rotateTitle = function() {
     //setup
-    var self = this;
+    var self = this,
+        vars = {};
     
     function trim(str) {
       return str.replace(/^\s+|\s+$/g,"");
     }
     function setCurrentIndex(index) {
-      self.data('rtIndex', index);
+      vars.rtIndex = index;
       return true;
     }
     function getThingList() {
-      if (self.data('thingList')) {return self.data('thingList')}
+      if (vars.thingList) {return vars.thingList}
       
       var things = self.attr('title').split(',');
-      self.data('thingList', things);
+      vars.thingList = things;
       return things;
     }
     function getNextThing() {
-      var index = parseInt(self.data('rtIndex')) + 1 ,
+      var index = parseInt(vars.rtIndex) + 1 ,
           thingList = getThingList();
       
       if (typeof thingList[index] == "undefined") {index = 0}
@@ -37,16 +38,42 @@
       
       return trim(thingList[index]);
     }
+    function makeThingElement(){
+      var newThingElement = self.clone();
+      newThingElement.html(getNextThing());
+      return newThingElement;
+    }
+    function showNewThing() {
+      var newThing = makeThingElement(),
+          container = self.parent();
+
+      newThing.css({left:'8em', opacity:0});
+      container.append(newThing);
+
+      newThing.animate({opacity:1,left:'5.4em'}, 700);
+      return newThing;
+    }
+    function hideOldThing() {
+      self.animate({opacity:0,top:'-1em'}, function(){$(this).remove()})
+    }
+    function cycle(){
+      var newThing = showNewThing();
+      hideOldThing();
+      
+      self = newThing;
+    }
+    
     function setup() {
       setCurrentIndex(0);
+      
     }
     
     /* main */
     setup();
     
     setInterval(function(){
-      console.dir(getNextThing());
-    }, 1000);
+      cycle();
+    }, 5000);
       
   }
 })(jQuery)
