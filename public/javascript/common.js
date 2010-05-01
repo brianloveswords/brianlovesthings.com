2ec419a4aa4c37c8ca147fff8e661886cfcda162
Brian Brennan <me@brianlovesthings.com>
2010-05-01 19:31:50 +0000

    fixed array.shuffle, added more js methods and tests.

--- public/javascript/common.js
+++ public/javascript/common.js
@@ -2,22 +2,45 @@ String.prototype.trim = function () {
   return this.replace(/^\s+|\s+$/g,"");
 };
 
+Array.prototype.isArray = true;
+
 Array.prototype.shuffle = function() {
   // based on the Knuth shuffle algorithm
   var max = this.length,
       randomIndex = function(n){return Math.floor(Math.random()*n)},
-      shuffled = new Array(max);
+      arr = this.clone();
       
-  for(var i = max; i > 1; i--){
+  for (var i = max; i > 1; i--){
     var rnd = randomIndex(i),
-        tmp = this[rnd];
+        tmp = arr[rnd];
         
-    shuffled[i-1] = this[i-1];
-    shuffled[rnd] = this[rnd];
-        
-    shuffled[rnd] = shuffled[i-1];
-    shuffled[i-1] = tmp;
-
+    arr[rnd] = arr[i-1];
+    arr[i-1] = tmp;
   }
-  return shuffled;
+  return arr;
+};
+
+
+Array.prototype.contains = function(obj) {
+  var max = this.length;
+  for (var i=0; i<max; i++) {
+    if (this[i] == obj) { return true; }
+  }
+  return false;
+};
+
+
+Array.prototype.same = function(arr) {
+  return (arr.join() == this.join());
+};
+  
+
+Array.prototype.clone = function() {
+  var max = this.length,
+      newArr = new Array(max);
+  
+  for (var i=0; i<max; i++) {
+    newArr[i] = this[i];
+  }
+  return newArr;
 }
--- public/javascript/tests/common.js
+++ public/javascript/tests/common.js
@@ -1,12 +1,39 @@
-module( "native extensions" );
+module( "String extensions" );
+
 test("Whitespace trimming", function() {
   expect(1);
-  equals("  fffuuu  ".trim(), "fffuuu", "But the whitespace remains the same");
+  equals("  fffuuu  ".trim(), "fffuuu", "Whitspace should be trimmed from both sides");
 });
-  
+
+module( "Array extensions" );
+
 test("Array shuffling", function() {
+  expect(2);
+  
+  var arr = [1,"test",{a:'b'},"four" ["five"]],
+      shuffled = arr.shuffle();
+  
+  ok( shuffled != arr, "Array should be shuffled");
+  ok( shuffled.sort().same( arr.sort() ) , "Array still contains all items");
+});
+
+test("Array contains", function() {
   expect(1);
-  arr = [1,2,3,4,5,6];
-  ok( arr.shuffle() != arr, "The array remains unshuffled");
+  
+  var arr = ['0', 'test'];
+  ok( arr.contains('test'), "obj should be found in array" );
 });
     
+test('Array comparison', function() {
+  expect(2);
+  ok( [1,2,[3,4]].same( [1,2,[3,4]] ), "array.same() should return true");
+  ok( [1,2,[3,4]].same( [1,2,[3,9]] ) == false , "array.same() should return false");
+});
+
+test("Array clone", function() {
+  expect(2);
+  
+  var arr = ['test', 1, [9, 10]];
+  ok( arr.clone() !== arr, "array should not be itself" );
+  ok( arr.clone().same( arr ), "array should be cloned" );
+});
